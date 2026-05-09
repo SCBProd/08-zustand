5fad630bd546dc83e175eae12257b55903764bf6
SCBProd <scbproduktion@gmail.com>
2026-05-09 14:41:46 +0200

    build3

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,13 +1,31 @@
 ///app\notes\filter\[...slug]\page.tsx
+import {
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+} from "@tanstack/react-query";
 
-import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api";
+import type { Tag } from "@/types/note";
 
 type Props = {
   params: Promise<{
     slug: string[];
   }>;
+};
+
+// type guard
+const isTag = (value?: string): value is Tag => {
+  if (!value) return false;
+
+  return (
+    value === "Work" ||
+    value === "Personal" ||
+    value === "Meeting" ||
+    value === "Shopping" ||
+    value === "Todo"
+  );
 };
 
 export default async function NotesPage({ params }: Props) {
@@ -18,22 +36,26 @@ export default async function NotesPage({ params }: Props) {
   const page = 1;
   const perPage = 12;
   const search = "";
-  const tag = slug?.[0] !== "all" ? slug?.[0] : "";
+
+  const rawTag = slug?.[0];
+  const tag = rawTag === "all" ? undefined : rawTag;
+
+  const safeTag = isTag(tag) ? tag : undefined;
 
   await queryClient.prefetchQuery({
-    queryKey: ["notes", page, perPage, search, tag],
+    queryKey: ["notes", page, perPage, search, safeTag],
     queryFn: () =>
       fetchNotes({
         page,
         perPage,
         search,
-        tag: tag || undefined,
+        tag: safeTag,
       }),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient tag={tag} />
+      <NotesClient tag={safeTag} />
     </HydrationBoundary>
   );
 }
